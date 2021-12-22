f1a76299d3beaf6f312c75e139648fb8dee9933c
eduardoagr <egomezr@outlook.com>
2021-12-22 01:05:54 +0100

    update

--- DuoNotes/Model/Note.cs
+++ DuoNotes/Model/Note.cs
@@ -1,8 +1,6 @@
 ﻿namespace DuoNotes.Model {
     public class Note : Notebook {
 
-        public new string Id { get; set; }
-
         public string NotebookId { get; set; }
 
         public string FileLocation { get; set; }
