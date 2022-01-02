3113d047aed6a65b682493a71e9ef7a1b9d31137
eduardoagr <egomezr@outlook.com>
2022-01-02 21:07:43 +0100

    Revert "update"
    This reverts commit f1a76299d3beaf6f312c75e139648fb8dee9933c.

--- DuoNotes/Model/Note.cs
+++ DuoNotes/Model/Note.cs
@@ -1,6 +1,8 @@
 ﻿namespace DuoNotes.Model {
     public class Note : Notebook {
 
+        public new string Id { get; set; }
+
         public string NotebookId { get; set; }
 
         public string FileLocation { get; set; }
