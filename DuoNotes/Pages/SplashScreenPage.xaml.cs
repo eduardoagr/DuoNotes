b13f356e782bbcade9c9dba59fcf15b84799e8d4
eduardoagr <egomezr@outlook.com>
2022-02-23 15:18:47 +0100

    Cleon up,y making a SpashScreenViewModel, make that inherit from a Page

--- DuoNotes/Pages/SplashScreenPage.xaml.cs
+++ DuoNotes/Pages/SplashScreenPage.xaml.cs
@@ -14,19 +14,6 @@ namespace DuoNotes.Pages {
         public SplashScreenPage() {
             InitializeComponent();
         }
-
-        private void AnimationView_OnFinishedAnimation(object sender, EventArgs e) {
-
-            var UserID = Preferences.Get(AppConstant.UserID, string.Empty);
-
-            if (!string.IsNullOrEmpty(UserID)) {
-                Application.Current.MainPage = new NavigationPage(new NotebooksPage());
-            } else {
-                Application.Current.MainPage = new NavigationPage(new LoginPage());
-            }
-        }
-
-        // This is only to make sure the user do not press the back button
         protected override bool OnBackButtonPressed() {
             return true;
         }
---
+++ DuoNotes/ViewModel/SplashScreenPageModel.cs
@@ -0,0 +1,35 @@
+﻿using DuoNotes.Constants;
+using DuoNotes.View;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace DuoNotes.ViewModel {
+    internal class SplashScreenPageModel : Page {
+
+        public Command FinishedAnimtionCommand { get; set; }
+
+        public SplashScreenPageModel() {
+
+            FinishedAnimtionCommand = new Command(OnFinishedAction);
+        }
+
+        private void OnFinishedAction() {
+            var UserID = Preferences.Get(AppConstant.UserID, string.Empty);
+
+            if (!string.IsNullOrEmpty(UserID)) {
+                Application.Current.MainPage = new NavigationPage(new NotebooksPage());
+            } else {
+                Application.Current.MainPage = new NavigationPage(new LoginPage());
+            }
+        }
+
+        protected override bool OnBackButtonPressed() {
+            return true;
+        }
+    }
+}
